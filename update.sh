7a08a2f9fe662b8cac86e61a0498ff475981b103
Ben <chuben@outlook.com>
2024-03-27 12:43:23 +0800

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -4,10 +4,12 @@
 [ -f "/q/.env" ] && source /q/.env
 [ -f "/q/install.conf" ] && source /q/install.conf
 
-[ -z "$accessToken" ] || [ -z "$alias" ] || [ -z "$payoutId" ] || [ -z "$pushUrl" ] && exit
+[ -z "$accessToken" ] || [ -z "$minerAlias" ] || [ -z "$payoutId" ] || [ -z "$pushUrl" ] && exit
 
 apt update -y && apt install wget jq curl -y
 
-alias="$(echo $alias | awk -F '-' '{print $1}'|awk -F '_' '{print $1}')"
+alias="$(echo $minerAlias | awk -F '-' '{print $1}'|awk -F '_' '{print $1}')"
 
 bash <(wget -qO- https://raw.githubusercontent.com/chuben/script/main/qli-monitor.sh ) --access-token $accessToken --miner-alias $alias --payout-id $payoutId --push-url $pushUrl --install
+
+systemctl restart qli
