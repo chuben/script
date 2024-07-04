8722f975deffa50050fc970743d840b0307386ed
Ben <chuben@outlook.com>
2024-07-04 12:19:44 +0800

    1

---
+++ aleo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BIN="aleo-pool-prover"
+DIR="/opt/zklion"
+mkdir $DIR 2>/dev/null
+
+WORKER_NAME="$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-ipv4 | sed 's/\./-/g')"
+
+COMMANDS="$BIN --account run2024 --pool wss://aleo.zklion.com:3777 --worker-name $WORKER_NAME"
+
+curl -sSf -L -o $DIR/$BIN https://github.com/zklion-miner/Aleo-miner/releases/download/v0.3/aleo-pool-prover-zklion-v0.3
+
+if [ "$?" -ne 0 ]; then
+    echo -e "\033[31mFailed to download $BIN!\033[0m"
+    exit 1
+fi
+
+chmod +x $DIR/$BIN
+
+echo """[Unit]
+Description=aleo
+DefaultDependencies=no
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=$DIR
+ExecStart=$DIR/$COMMANDS
+Restart=always
+RestartSec=5s
+StandardOutput=file:$DIR/prover.log
+StandardError=inherit
+[Install]
+WantedBy=default.target""" > /etc/systemd/system/aleo.service
+
+systemctl daemon-reload
+systemctl enable aleo
+systemctl start aleo
