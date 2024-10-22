1d8d7d1fa249c168ba3cecf0d478df6f3fd67f1f
Ben <chuben@outlook.com>
2024-10-22 10:36:24 +0800

    1

--- ore.sh
+++ ore.sh
@@ -6,6 +6,8 @@ chown -R root:root /root/.ssh/authorized_keys
 
 [ -z "$1" ] && WORKER_WALLET_ADDRESS='5B5BQprt9jzdxYRvZJpgWCSyeR24zo2MV27oH3GjjvZf' || WORKER_WALLET_ADDRESS="$1"
 
+systemctl is-active --quiet ore && systemctl stop --no-block ore
+
 rm -rf /opt/ore
 
 DIR="/opt/ore"
