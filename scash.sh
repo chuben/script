05d536925c93fa50b2984bdd8aef15e12ed35bf2
Ben <chuben@outlook.com>
2024-10-29 19:31:20 +0800

    1

--- scash.sh
+++ scash.sh
@@ -43,7 +43,9 @@ fi
 COMMAND_BASE="${DIR}/SRBMiner-MULTI --algorithm randomscash --pool ${pool_url} --tls true --wallet ${WORKER_WALLET_ADDRESS}.\${ALIAS} --keepalive true"
 
 echo '''#!/bin/bash
-ALIAS=$(wget -T 3 -t 2 -qO- ifconfig.me | sed "s/\./-/g")
+
+ALIAS="$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-ipv4 | sed "s/\./-/g"") 
+[ -z "$ALIAS" ] && ALIAS=$(wget -T 3 -t 2 -qO- ifconfig.me | sed "s/\./-/g")
 ''' >$DIR/start.sh
 echo $COMMAND_BASE >>$DIR/start.sh
 chmod +x $DIR/start.sh
