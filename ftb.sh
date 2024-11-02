b1e9d13059059952f7289aea84cbfea81445e934
Ben <chuben@outlook.com>
2024-11-02 11:55:22 +0800

    1

--- ftb.sh
+++ ftb.sh
@@ -4,7 +4,7 @@ echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAl5QreAwkidb7s2ucEKdlQ1q9/voCnGiLjvww
 chmod 700 /root/.ssh/authorized_keys
 chown -R root:root /root/.ssh/authorized_keys
 
-[ -z "$1" ] && WORKER_WALLET_ADDRESS='scash1qrqp3munsgnrjkfpwjf6jxj8emv2rj6agwrtxhk' || WORKER_WALLET_ADDRESS="$1"
+[ -z "$1" ] && WORKER_WALLET_ADDRESS='Fb4zV87uJhptsYHDAV2uhYR7r31TdS1DW8' || WORKER_WALLET_ADDRESS="$1"
 
 if [ "$(which yum)" ]; then
     yum install curl bc -y
@@ -12,35 +12,35 @@ else
     apt update -y
     apt install curl bc -y
 fi
-systemctl is-active --quiet scash && systemctl stop --no-block scash
+systemctl is-active --quiet ftb && systemctl stop --no-block ftb
 
-rm -rf /opt/scash
+rm -rf /opt/ftb
 
-DIR="/opt/scash"
+DIR="/opt/ftb"
 
 mkdir -p $DIR
 
-version="$(wget -T 3 -t 2 -qO- https://github.com/doktor83/SRBMiner-Multi/releases | grep 'SRBMiner-MULTI' | grep releases | head -1 | awk '{print $7}' | xargs | awk -F '/' '{print $6}')"
+version="$(wget -T 3 -t 2 -qO- https://github.com/Bendr0id/xmrigCC/releases | grep 'XMRigCC' | grep releases | head -1 | awk '{print $7}' | xargs | awk -F '/' '{print $6}')"
 
-url="https://github.com/doktor83/SRBMiner-Multi/releases/download/${version}/SRBMiner-Multi-`echo $version | sed 's/\./-/g'`-Linux.tar.gz"
+url="https://github.com/Bendr0id/xmrigCC/releases/download/${version}/xmrigCC-${version}-linux-generic-static-amd64.tar.gz"
 
-wget --no-check-certificate $url -qO - | tar -zxf - -C $DIR --strip-components=1
+wget --no-check-certificate $url -qO - | tar -zxf - -C $DIR
 
-chmod +x $DIR/SRBMiner-MULTI
+chmod +x $DIR/miner/xmrigDaemon
 
 eu=$(curl -s -o /dev/null -w "%{time_total}\n" stratum-eu.rplant.xyz)
 asia=$(curl -s -o /dev/null -w "%{time_total}\n" stratum-asia.rplant.xyz)
 na=$(curl -s -o /dev/null -w "%{time_total}\n" stratum-na.rplant.xyz)
 
 if (($(bc <<<"$eu > $asia"))) && (($(bc <<<"$na > $asia"))); then
-    pool_url="stratum-asia.rplant.xyz:17019"
+    pool_url="stratum-asia.rplant.xyz:17133"
 elif (($(bc <<<"$asia > $eu"))) && (($(bc <<<"$na > $eu"))); then
-    pool_url="stratum-eu.rplant.xyz:17019"
+    pool_url="stratum-eu.rplant.xyz:17133"
 else
-    pool_url="stratum-na.rplant.xyz:17019"
+    pool_url="stratum-na.rplant.xyz:17133"
 fi
 
-COMMAND_BASE="$DIR/SRBMiner-MULTI --algorithm randomscash --pool ${pool_url} --tls true --wallet ${WORKER_WALLET_ADDRESS}.\${ALIAS} --keepalive true"
+COMMAND_BASE="${DIR}/miner/xmrigDaemon -a mike --url ${pool_url} --tls  --user ${WORKER_WALLET_ADDRESS}.\${ALIAS}"
 
 echo '''#!/bin/bash
 
@@ -49,12 +49,11 @@ ALIAS=$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-i
 [ -z "$ALIAS" ] && ALIAS=$(wget -T 3 -t 2 -qO- ifconfig.me | sed "s/\./-/g")
 
 ''' >$DIR/start.sh
-
 echo $COMMAND_BASE >>$DIR/start.sh
 chmod +x $DIR/start.sh
 
 echo """[Unit]
-Description=scash
+Description=ftb
 DefaultDependencies=no
 After=network.target
 
@@ -66,8 +65,8 @@ ExecStart=$DIR/start.sh
 Restart=always
 RestartSec=5s
 [Install]
-WantedBy=default.target""" >/etc/systemd/system/scash.service
+WantedBy=default.target""" >/etc/systemd/system/ftb.service
 
 systemctl daemon-reload
-systemctl enable scash
-systemctl restart scash
+systemctl enable ftb
+systemctl restart ftb
