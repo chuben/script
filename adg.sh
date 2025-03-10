54cab1289f06c9b9030b68459eddd9e21da19b46
Ben <chuben@outlook.com>
2025-03-10 21:26:38 +0800

    1

---
+++ adg.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
+echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
+
+apt update -y
+version=`uname -r|awk -F '-cloud-' '{print $1}'`
+apt install linux-headers-$version-amd64 linux-image-$version-amd64 certbot python3-pip ppp iptables-persistent netfilter-persistent  net-tools curl -y
+
+[ "$?" -eq 0 ] && rm -rf /boot/*cloud*
+
+update-grub
+
+echo '''name sstpd
+require-mschap-v2
+nologfd
+nodefaultroute
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4''' > /etc/ppp/options.sstpd 
+
+echo """# Secrets for authentication using CHAP
+# client        server  secret                  IP addresses
+ben * $1 *
+vm * $1 192.168.88.200""" > /etc/ppp/chap-secrets
+
+pip3  install sstp-server
+
+sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
+sysctl -p
+
+iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -o ens5 -j MASQUERADE
+
+netfilter-persistent save
+
+echo '''#!/bin/bash
+sstpd -c  fullchain.pem -k privkey.pem  --local 192.168.88.1 --remote 192.168.88.0/24 -p 9443 -l 0.0.0.0''' > /opt/sstp/start.sh 
+
+cd /opt/sstp/
+chmod +x start.sh 
+
+echo '''[Unit]
+Description=sstp
+
+[Service]
+#Type=forking
+WorkingDirectory=/opt/sstp
+PrivateTmp=true
+ExecStart=/opt/sstp/start.sh
+
+[Install]
+WantedBy=multi-user.target''' > /etc/systemd/system/sstp.service
+
+wget  -T 3 -t 2 -qO-  https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.57/AdGuardHome_linux_386.tar.gz | tar -zxf - -C /opt/ 
+chmod +x /opt/AdGuardHome/AdGuardHome
+
+nano /opt/AdGuardHome/AdGuardHome.yaml
+
+echo '''[Unit]
+Description=AdGuardHome
+
+[Service]
+#Type=forking
+WorkingDirectory=/opt/AdGuardHome
+PrivateTmp=true
+ExecStart=/opt/AdGuardHome/AdGuardHome
+
+[Install]
+WantedBy=multi-user.target''' > /etc/systemd/system/AdGuardHome.service
+
+systemctl daemon-reload
+systemctl stop systemd-resolved
+systemctl disable systemd-resolved
+systemctl enable sstp AdGuardHome
+
+reboot
--- squid.sh
+++ squid.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-apt-get install -y squid
+bash <(wget -qO- -o- https://git.io/v2ray.sh)
 
-# 备份原始配置文件
-cp /etc/squid/squid.conf /etc/squid/squid.conf.bak
+rm -rf /etc/v2ray/conf/*
 
-# 创建新的配置文件
-cat << EOF | tee /etc/squid/squid.conf
-# 基本配置
-http_port 29999
-http_access allow all
-dns_nameservers 1.1.1.1
-cache_dir ufs /var/spool/squid 100 16 256
-coredump_dir /var/spool/squid
-EOF
+echo '{
+  "inbounds": [
+    {
+      "tag": "Socks5.json",
+      "port": 29999,
+      "listen": "0.0.0.0",
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "ip": "0.0.0.0"
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      }
+    }
+  ]
+}' > /etc/v2ray/conf/Socks5.json
 
-# 重启Squid服务以应用配置
-systemctl restart squid
-
-# 设置Squid开机自启
-systemctl enable squid
+v2ray restart
