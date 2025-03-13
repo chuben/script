590e31b85c18d85d3d34aed47d174be9a4c59bb6
Ben <chuben@outlook.com>
2025-03-13 13:40:52 +0800

    1

--- squid.sh
+++ squid.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo '''
-net.ipv4.ip_forward=1
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-''' >  /etc/sysctl.conf 
-sysctl -p
 
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 chmod +x cloudflared-linux-amd64
@@ -44,19 +38,35 @@ systemctl restart cloudflared
 
 systemctl enable cloudflared
 
-apt-get install -y squid
+echo 'nameserver 127.0.0.1' > /etc/resolv.conf 
+chattr +i /etc/resolv.conf 
+bash <(wget -qO- -o- https://git.io/v2ray.sh)
+v2ray del
 
-cat << EOF | sudo tee /etc/squid/squid.conf
-# 基本配置
-http_port 29999
-http_access allow all
-
-# 强制使用127.0.0.53作为DNS服务器
-dns_nameservers 127.0.0.1
-
-# 默认配置保持不变
-cache_dir ufs /var/spool/squid 100 16 256
-coredump_dir /var/spool/squid
+# 创建新的配置文件
+cat << EOF | tee /etc/v2ray/conf/Socks-29999.json
+{
+  "inbounds": [
+    {
+      "tag": "Socks-29999.json",
+      "port": 29999,
+      "listen": "0.0.0.0",
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "ip": "0.0.0.0"
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      }
+    }
+  ]
+}
 EOF
-systemctl restart squid
-systemctl enable squid
+
+v2ray restart 
