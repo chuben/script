3766de6b439e32b74780f6e56b044269b13744e0
Ben <chuben@outlook.com>
2025-04-16 08:18:29 +0800

    1

--- bitz-pool.sh
+++ bitz-pool.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-SERVER_URL="http://mine.oreminepool.top:8080/"
-WALLET_ADDR="6Vtcd2cAq5sjr4FThMGmVvTUPtTbo8L9xJjrTxYa9dxC"
+SERVER_URL="http://minebitz1.oreminepool.top:8880/"
+WALLET_ADDR="SbxQm8HWUQefijEd8KDJkQMKbnpxvEDdBNweDXhH6T6"
 INSTALL_DIR="/opt/bitz"
 SERVICE_NAME="bitz"
 
@@ -14,18 +13,32 @@ mkdir -p "$INSTALL_DIR"
 
 cd $INSTALL_DIR
 wget -q --show-progress https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512
+
+echo '''#!/bin/bash
+ip="$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-ipv4)"
+[ -z "$ip" ] && exit 1
+instype=$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/instance-type| sed "s/xlarge//g"|sed "s/\.//g")
+[ -z "$instype" ] && exit 1
+country=$(wget -T 3 -t 2 -qO - http://169.254.169.254/2021-03-23/meta-data/placement/availability-zone|cut -b 1-2 )
+[ -z "$country" ] && exit 1
+minerAlias=${country}_${instype}_$ip
+''' > $INSTALL_DIR/start.sh
+
+echo "$INSTALL_DIR/ore-mine-pool-linux-avx512 worker --route-server-url '$SERVER_URL'  --server-url 'bitz' --worker-wallet-address '$WALLET_ADDR' --alias \$minerAlias">> $INSTALL_DIR/start.sh
+
 chmod +x ore-mine-pool-linux-avx512
+chmod +x $INSTALL_DIR/start.sh
 
 cat > /etc/systemd/system/${SERVICE_NAME}.service <<EOF
 [Unit]
-Description=ORE Mining Pool Worker
+Description=BITZ Mining Pool Worker
 After=network.target
 
 [Service]
 Type=simple
 User=root
 WorkingDirectory=$INSTALL_DIR
-ExecStart=$INSTALL_DIR/ore-mine-pool-linux-avx512 worker --server-url $SERVER_URL --worker-wallet-address $WALLET_ADDR
+ExecStart=$INSTALL_DIR/start.sh
 Restart=always
 RestartSec=30
 StandardOutput=syslog
@@ -35,7 +48,6 @@ Environment="LD_LIBRARY_PATH=$INSTALL_DIR"
 [Install]
 WantedBy=multi-user.target
 EOF
-
 systemctl daemon-reload
 systemctl enable $SERVICE_NAME
 systemctl restart $SERVICE_NAME
