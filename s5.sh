4c740cee3700dcad64c919a30825e082d7620851
admin <chuben@outlook.com>
2025-07-21 09:20:19 +0800

    1

--- s5.sh
+++ s5.sh
@@ -8,7 +8,11 @@ apt update && apt install -y bc wget
 
 ip="$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-ipv4)"
 
-[ "$ip" ] && port=$(echo "5000 + $(echo $ip | awk -F '.' '{print $4}')" | bc) || port=5000
+[ -z "$ip" ] && ip="$(wget -T 3 -t 2 -qO- https://icanhazip.com)"
+
+[ -z "$ip" ] && exit
+
+port=$(echo "5000 + $(echo $ip | awk -F '.' '{print $4}')" | bc)
 
 [ "$1" ] && pwd="$1" || pwd="bd2de253d21979799315004c01801df0"
 
