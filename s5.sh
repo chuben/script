12c24cf82ef2fc8eae144d5d8b4ef3ad39a0498b
admin <chuben@outlook.com>
2025-07-21 08:39:53 +0800

    1

--- s5.sh
+++ s5.sh
@@ -2,6 +2,8 @@
 
 bash <(wget -qO- https://git.io/v2ray.sh)
 
+v2ray del *
+
 apt update && apt install -y bc wget
 
 ip="$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-ipv4)"
