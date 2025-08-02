ec4b9dd5fc9ef4e3f90a6f5bf25797d276dadc7a
admin <chuben@outlook.com>
2025-08-02 18:26:32 +0800

    1

--- wg.sh
+++ wg.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # 参数检查
 if [ -z "$1" ]; then
@@ -26,7 +25,9 @@ apt install wireguard resolvconf curl wget openssl -y
 
 # 获取网卡和公网 IP 第四段
 nic_name=$(ip route | grep default | head -1 | awk '{print $5}')
-ip=$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-ipv4 | awk -F '.' '{print $4}')
+ip=$(wget -T 3 -t 2 -qO- http://169.254.169.254/2021-03-23/meta-data/public-ipv4)
+ip_last1=$(echo $ip|awk -F '.' '{print $3}')
+ip_last2=$(echo $ip| awk -F '.' '{print $4}')
 
 # 加密后的密钥（请替换为你自己的加密后值）
 ENC_PRIVATE_KEY="U2FsdGVkX1/1da1pfoZqKVefnSF5doyWLCWMu5Jl5tufWLNmP5zQVxbsdW+KlhxX +mMJgjJMOEkL6O4IkK70WQ=="
@@ -40,16 +41,16 @@ PEER_PUB_KEY=$(echo "$ENC_PEER_PUB_KEY" | openssl enc -aes-256-cbc -a -d -salt -
 cat > /etc/wireguard/wg0.conf <<EOF
 [Interface]
 PrivateKey = $PRIVATE_KEY
-Address = 10.0.8.1/24
+Address = 10.${ip_last1}.${ip_last2}.1/24,fe80:25bd:7d15:38b9::1/128
 PostUp   = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $nic_name -j MASQUERADE
 PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $nic_name -j MASQUERADE
-ListenPort = 50${ip}
+ListenPort = 50${ip_last2}
 DNS = 1.1.1.2,1.1.1.3
 MTU = 1420
 
 [Peer]
 PublicKey = $PEER_PUB_KEY
-AllowedIPs = 10.0.8.10/24
+AllowedIPs = 10.${ip_last1}.${ip_last2}.10/24,fe80:25bd:7d15:38b9::10/128
 EOF
 
 # 启动 WireGuard
