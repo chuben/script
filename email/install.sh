1caef863ac0474fc14381b734e29ada1c2c210d8
admin <chuben@outlook.com>
2025-08-05 08:18:59 +0800

    1

---
+++ email/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+apt update -y
+apt install wget -y
+
+mkdir -p "/opt/email"
+
+wget -qO /opt/email/simple_mail_server https://raw.githubusercontent.com/chuben/script/main/email/simple_mail_server
+
+chmod +x /opt/email/simple_mail_server
+
+cat > /etc/systemd/system/simple_mail_server.service <<EOF
+[Unit]
+Description=Simple Mail Server (Flask + SMTP)
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/opt/email
+ExecStart=/opt/email/simple_mail_server
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable simple_mail_server
+systemctl restart simple_mail_server
