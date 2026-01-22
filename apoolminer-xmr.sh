046f889a20552b281d97feead342df42748ee3ec
ben <chuben@outlook.com>
2026-01-22 17:18:38 +0800

    1

--- apoolminer-xmr.sh
+++ apoolminer-xmr.sh
@@ -30,13 +30,10 @@ wget -qO- "$DOWNLOAD_URL" | tar -zxf - -C "$INSTALL_DIR" --strip-components=1
 cat > "$INSTALL_DIR/update.sh" <<EOF
 #!/bin/bash
 LAST_VERSION=\$(wget -qO- https://api.github.com/repos/apool-io/apoolminer/releases/latest | jq -r .tag_name | cut -b 2-)
-LOCAL_VERSION=\$("$INSTALL_DIR"/apoolminer --version | awk '{print \$2}')
-[ "\$LAST_VERSION" == "\$LOCAL_VERSION" ] && echo '无更新' && exit 0
-echo "\$LAST_VERSION" | awk -F . '{print \$1\$2\$3, "LAST_VERSION"}' > /tmp/versions
-echo "\$LOCAL_VERSION" | awk -F . '{print \$1\$2\$3, "LOCAL_VERSION"}' >> /tmp/versions
-NEW_VERSION=\$(sort -n /tmp/versions | tail -1 | awk '{print \$2}')
+LOCAL_VERSION=\$("/opt/apoolminer"/apoolminer --version | awk '{print \$2}')
+[ "\$(printf '%s\n%s\n' "\$LAST_VERSION" "\$LOCAL_VERSION" | sort -V | tail -n1)" == "\$LOCAL_VERSION" ] && echo '无更新' && exit 0
 [ "\$NEW_VERSION" == "\$LOCAL_VERSION" ] && exit 0
-bash <(wget -qO- https://raw.githubusercontent.com/chuben/script/main/apoolminer.sh) "$ACCOUNT"
+bash <(wget -qO- https://raw.githubusercontent.com/chuben/script/main/qubic_xmr.sh) "$ACCOUNT"
 EOF
 
 chmod +x "$INSTALL_DIR/update.sh"
