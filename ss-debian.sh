f33f70c1e708f64cd87d8819d9aef422dac1301c
Ben <chuben@outlook.com>
2026-04-02 16:56:50 +0800

    1

--- ss-debian.sh
+++ ss-debian.sh
@@ -8,4 +8,6 @@ systemctl stop ss-rust shadow-tls && systemctl disable ss-rust shadow-tls
 
 bash <(wget -qO- -o- https://github.com/233boy/sing-box/raw/main/install.sh)
 
+sing-box del *
+
 sing-box add Trojan 443 "$KEY"
