994980d3aaf7df582ff13da8c309903db04e6e2d
Ben <chuben@outlook.com>
2026-06-03 12:00:08 +0800

    1

--- adg.sh
+++ adg.sh
@@ -1,85 +1,190 @@
 #!/bin/bash
+set -e
 
-[ -z "$1" ] && exit || psw=$1
+########################################
+# 参数检查
+########################################
+
+[ -z "$1" ] && {
+    echo "Usage: $0 <sstp_password>"
+    exit 1
+}
+
+PSW="$1"
+SUBNET_ID=$((RANDOM % 254 + 1))
+SUBNET="172.16.${SUBNET_ID}.0/24"
+GATEWAY="172.16.${SUBNET_ID}.1"
 
 export DEBIAN_FRONTEND=noninteractive
+
+########################################
+# iptables-persistent 预设
+########################################
+
 echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
 echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
 
-vmlinuz_version=`ls /boot/vmlinuz* |tail -1 |awk -F '-' '{print $2"-"$3}'`
-apt update -y
-apt install -y linux-headers-${vmlinuz_version}-amd64 linux-image-${vmlinuz_version}-amd64 \
-    iptables-persistent netfilter-persistent  net-tools curl \
-    certbot python3-certbot-nginx git build-essential libssl-dev zlib1g-dev libbz2-dev \
-    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-    libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
-    python3-openssl ppp
+########################################
+# 安装系统组件
+########################################
 
-[ "$?" -eq 0 ] && rm -rf /boot/*cloud*
+apt update
 
-update-grub
+if uname -r | grep -q cloud; then
+    echo "Cloud kernel detected, installing standard kernel..."
 
-echo '''name sstpd
+    apt install -y \
+        linux-image-amd64 \
+        linux-headers-amd64
+
+    rm -rf /boot/*-cloud-*
+
+    update-grub
+
+    echo "Kernel switched. Reboot required."
+fi
+
+apt install -y \
+    iptables-persistent \
+    netfilter-persistent \
+    net-tools \
+    curl \
+    ppp \
+    openssl
+
+
+########################################
+# PPP 配置
+########################################
+
+cat >/etc/ppp/options.sstpd <<EOF
+name sstpd
 require-mschap-v2
 nologfd
 nodefaultroute
-ms-dns 192.168.84.1''' > /etc/ppp/options.sstpd 
+ms-dns 1.1.1.1
+EOF
 
-echo """# Secrets for authentication using CHAP
-# client        server  secret                  IP addresses
-ben * $psw *
-vm * $psw 192.168.84.200""" > /etc/ppp/chap-secrets
+cat >/etc/ppp/chap-secrets <<EOF
+# client server secret IP
+m * ${PSW} *
+EOF
 
-curl https://pyenv.run | bash
+########################################
+# 安装 Micromamba
+########################################
 
+curl -Ls https://micro.mamba.pm/install.sh | bash
 
-echo '''export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"''' >> ~/.bashrc 
+export MAMBA_ROOT_PREFIX=/root/micromamba
+eval "$(/root/.local/bin/micromamba shell hook -s bash)"
 
-source ~/.bashrc 
-pyenv install 3.9.7
+micromamba create -y -n py39 python=3.9
 
-pyenv global 3.9.7
+########################################
+# 安装 SSTP Server
+########################################
 
-certbot --nginx --key-type rsa -d kr.newmanhub.com --non-interactive --agree-tos --email admin@newmanhub.com
-mkdir -p /opt/sstp/
+micromamba run -n py39 pip install -U pip
+micromamba run -n py39 pip install sstp-server
 
-cp -f /etc/letsencrypt/live/kr.newmanhub.com/* /opt/sstp/.
+mkdir -p /opt/sstp
+cd /opt/sstp
 
-pip3  install sstp-server
+########################################
+# 生成自签名证书
+########################################
 
-echo '''
+openssl req \
+    -x509 \
+    -newkey rsa:4096 \
+    -sha256 \
+    -days 3650 \
+    -nodes \
+    -keyout privkey.pem \
+    -out fullchain.pem \
+    -subj "/C=SG/ST=Singapore/L=Singapore/O=SSTP/CN=$(hostname -f)"
+
+chmod 600 privkey.pem
+
+########################################
+# 开启转发
+########################################
+
+cat >/etc/sysctl.conf <<EOF
 net.ipv4.ip_forward=1
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-''' >  /etc/sysctl.conf 
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+EOF
+
 sysctl -p
 
-net_name=`ip route | grep default|awk '{print $5}'`
-iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -o $net_name -j MASQUERADE
+########################################
+# NAT
+########################################
+
+WAN_IF=$(ip route | awk '/default/ {print $5; exit}')
+iptables -P FORWARD ACCEPT
+iptables -t nat -A POSTROUTING \
+    -s ${SUBNET} \
+    -o ${WAN_IF} \
+    -j MASQUERADE
 
 netfilter-persistent save
 
-echo '''#!/bin/bash
-/root/.pyenv/shims/sstpd -c  fullchain.pem -k privkey.pem  --local 192.168.84.1 --remote 192.168.84.0/24 -p 9443 -l 0.0.0.0''' > /opt/sstp/start.sh 
-cd /opt/sstp/
-chmod +x start.sh 
+########################################
+# 启动脚本
+########################################
 
-echo '''[Unit]
-Description=sstp
+cat >/opt/sstp/start.sh <<EOF
+#!/bin/bash
+
+export MAMBA_ROOT_PREFIX=/root/micromamba
+eval "\$(/root/.local/bin/micromamba shell hook -s bash)"
+
+micromamba activate py39
+
+exec /root/micromamba/envs/py39/bin/sstpd \
+    -c /opt/sstp/fullchain.pem \
+    -k /opt/sstp/privkey.pem \
+    --local ${GATEWAY} \
+    --remote ${SUBNET} \
+    -p 9443 \
+    -l 0.0.0.0
+EOF
+
+chmod +x /opt/sstp/start.sh
+
+########################################
+# systemd
+########################################
+
+cat >/etc/systemd/system/sstp.service <<EOF
+[Unit]
+Description=SSTP Server
+After=network-online.target
 
 [Service]
-#Type=forking
+Type=simple
 WorkingDirectory=/opt/sstp
-PrivateTmp=true
 ExecStart=/opt/sstp/start.sh
+Restart=always
+RestartSec=5
 
 [Install]
-WantedBy=multi-user.target''' > /etc/systemd/system/sstp.service
+WantedBy=multi-user.target
+EOF
 
 systemctl daemon-reload
 systemctl enable sstp
+
+echo
+echo "======================================"
+echo "SSTP 安装完成"
+echo "监听端口: 9443"
+echo "用户名: m"
+echo "密码: ${PSW}"
+echo "======================================"
+echo
 
 reboot
